ff40ad3195e7390a880a389fc410fa206643a25f
Jinho Chun <jinhochun@Jinhoui-Macmini.local>
2022-01-03 22:38:45 +0900

    Add Salt & Hash Multiple Rounds Using Bcrypt

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const app = express();
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
-
+const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -23,10 +24,6 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-userSchema.plugin(encrypt, {
-  secret: process.env.API_KEY,
-  encryptedFields: ["password"]
-});
 
 const User = new mongoose.model("User", userSchema);
 
@@ -41,50 +38,54 @@ app.route("/login")
     res.render("login");
   })
   .post(function(req, res) {
-    const username = req.body.username;
-    const password = req.body.password;
-    User.findOne({
-      email: username
-    }, function(err, foundUser) {
-      if (err) {
-        console.log(err);
-      } else {
-        if (foundUser) {
-          if (foundUser.password === password) {
-            res.render("secrets");
+      const username = req.body.username;
+      const password = req.body.password;
+      User.findOne({email: username}, function(err, foundUser) {
+          if (err) {
+            console.log(err);
           } else {
-            res.redirect("/login");
+            if (foundUser) {
+              bcrypt.compare(password, foundUser.password, function(err, result) {
+                if (result === true) {
+                  res.render("secrets");
+                } else {
+                  res.redirect("/login");
+                }
+              });
+            } else {
+              res.redirect("/login");
+            }
+          };
+      });
+    });
+
+
+    app.route("/register")
+    .get(function(req, res) {
+      res.render("register");
+    })
+    .post(function(req, res) {
+      bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        const newUser = new User({
+          email: req.body.username,
+          password: hash
+        });
+        newUser.save(function(err) {
+          if (err) {
+            console.log(err);
+          } else {
+            res.render("secrets");
           }
-        } else {
-          res.redirect("/login");
-        }
-      }
+        });
+      });
+
     });
-  });
 
-app.route("/register")
-  .get(function(req, res) {
-    res.render("register");
-  })
-  .post(function(req, res) {
-    const newUser = new User({
-      email: req.body.username,
-      password: req.body.password
+
+
+
+
+
+    app.listen(3000, function() {
+      console.log("Server started on port 3000.");
     });
-    newUser.save(function(err) {
-      if (err) {
-        console.log(err);
-      } else {
-        res.render("secrets");
-      }
-    });
-  });
-
-
-
-
-
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000.");
-});
